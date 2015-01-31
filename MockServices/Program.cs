b4f114bcd1c53e64bc0a58d7e9b9a2daba78a0e3
John Keith <johnkeith16@gmail.com>
2015-01-30 22:13:28 -0500

    Working.

--- MockServices/Program.cs
+++ MockServices/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Net;
 
 namespace MockServices
 {
@@ -6,7 +8,23 @@ namespace MockServices
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Requesting...");
+
+			// Note: Comment this line out to watch SSL fail.
+			ServicePointManager.ServerCertificateValidationCallback = (a,b,c,d) => true;
+
+			var webRequest = HttpWebRequest.Create (@"https://webservice.aspsms.com/aspsmsx2.asmx/VersionInfo");
+			using (var response = webRequest.GetResponse())
+			{
+				using (var stream = response.GetResponseStream())
+				{
+					using (var streamReader = new StreamReader(stream))
+					{
+						string result = streamReader.ReadToEnd();
+						Console.WriteLine(result);
+					}
+				}
+			}
 		}
 	}
 }
